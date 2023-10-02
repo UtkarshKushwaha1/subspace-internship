3c8de885c487e6314d8df8125e9fd37bc7a4ca4a
Utkarsh Kushwaha <utkarsh.kushwaha87@gmail.com>
2023-10-02 23:38:11 +0530

    done final changes

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const axios = require('axios');
 const middleware = require("./middleware");
 const app = express();
 const path = require('path');
@@ -13,35 +12,8 @@ app.get("/", middleware, (req, res) => {
   );
 });
 
-
-
-app.get('/search', (req, res) => {
+app.get('/api/blog-search', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-});
-
-
-app.get('/api/blog-search', async (req, res) => {
-  const query = req.query.query;
-
-  if (!query) {
-    return res.status(400).json({ error: 'Query parameter "query" is required.' });
-  }
-
-  try {
-    // Make a GET request to your blog API using Axios
-    const blogApiResponse = await axios.get(`YOUR_BLOG_API_ENDPOINT?q=${query}`);
-    const blogData = blogApiResponse.data;
-
-    // Assuming the API response contains an array of blog objects
-    const filteredBlogs = blogData.filter(blog =>
-      blog.title.toLowerCase().includes(query.toLowerCase())
-    );
-
-    res.json(filteredBlogs);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'An error occurred while fetching blog data.' });
-  }
 });
 
 
